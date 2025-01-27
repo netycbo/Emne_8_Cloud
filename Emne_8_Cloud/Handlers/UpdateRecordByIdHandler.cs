d891c6e3d46444f286f2c11f340ff8c1a7846caa
netyc <netyc@MIKI>
2025-01-27 21:09:24 +0100

    fixed bug

--- Emne_8_Cloud/Handlers/UpdateRecordByIdHandler.cs
+++ Emne_8_Cloud/Handlers/UpdateRecordByIdHandler.cs
@@ -7,20 +7,23 @@ namespace Emne_8_Cloud.Handlers
 {
     public class UpdateRecordByIdHandler(IRecordRepository repository) : IRequestHandler<UpdateRecordByIdRequest, UpdateRecordByIdResponse>
     {
+       
         public async Task<UpdateRecordByIdResponse> Handle(UpdateRecordByIdRequest request, CancellationToken cancellationToken)
         {
-            var record = repository.GetById(request.Id);
+            var record = await repository.GetById(request.Id);
             if (record != null)
             {
+                record.Title = request.Titel;
+                record.Description = request.Description;
                 await repository.UpdateRecordById(request.Id);
+                repository.SaveChanges();
+
                 return new UpdateRecordByIdResponse
                 {
-                    Message = "Record updated successfully nad now looks like this:",
+                    Message = "Record updated successfully and now looks like this:",
                     Titel = request.Titel,
                     Description = request.Description
-
                 };
-
             }
             return new UpdateRecordByIdResponse
             {
